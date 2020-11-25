09822555b3b6f1f2093fc7fee414f29025575586
drew-wright <drewwright31@gmail.com>
2020-11-24 22:04:24 -0600

    Added Game over and Input validator

--- rps.js
+++ rps.js
@@ -1,27 +1,33 @@
 function computerPick() {
     rockPaperScissors = ["rock", "paper", "scissors"]
     let i = Math.floor(Math.random()*3)
-    return[i];
+    return rockPaperScissors[i];
 }
 
 function Game(UserInput) {
-    let UserAnswer = UserInput.upper()
-    let ComputerAnswer = computerPick()
-    console.log('The Computer Chose: ' + ComputerAnswer)
-    console.log('You Chose: ' + UserAnswer)
-    if (ComputerAnswer == UserAnswer) {
-        console.log("It's a tie!")
-    }
-    if (ComputerAnswer === "rock" && UserAnswer === "paper") {
-        console.log("You Win!")
-    }
-    if (ComputerAnswer === "paper" && UserAnswer === "scissors"){
-        console.log("You Win!")
-    }
-    if (ComputerAnswer === "scissors" && UserAnswer === "rock"){
-        console.log("You Win!")
+    let UserAnswer = UserInput.toLowerCase()
+    if (UserAnswer == 'rock' || 'paper' || 'scissors') {
+        let ComputerAnswer = computerPick()
+        console.log('The Computer Chose: ' + ComputerAnswer)
+        console.log('You Chose: ' + UserAnswer)
+        if (ComputerAnswer == UserAnswer) {
+                console.log("It's a tie!")
+        }
+        else if (ComputerAnswer === "rock" && UserAnswer === "paper") {
+            console.log("You Win!")
+        }
+        else if (ComputerAnswer === "paper" && UserAnswer === "scissors"){
+            console.log("You Win!")
+        }
+        else if (ComputerAnswer === "scissors" && UserAnswer === "rock"){
+            console.log("You Win!")
+        }
+        else {
+            console.log("You Lose!")
+        }
+        return 'Game Over'
     }
     else {
-        console.log("You Lose!")
-    }
+        console.log("Please enter Rock, Paper, or Scissors")
+    }   
 }
